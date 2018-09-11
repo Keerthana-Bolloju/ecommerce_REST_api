dfd7a4be0c11659dce4db692c5653982c60caadf
Keerthana Bolloju <keerthigana4@gmail.com>
2018-09-11 12:51:58 +0530

    first commit

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "e-commerce App Documentation",
+  "version": "0.0.1",
+  "description": "Documentation for the Ecommerce app",
+  "title": "e-commerce APIs",
+  "url": "http://localhost:3000",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-09-11T07:19:34.683Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
